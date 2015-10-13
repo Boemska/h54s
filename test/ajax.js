f428e9a492a3918578ff5de20efd7c3f78b7d2fd
Bojan Djurdjevic <bojan88@gmail.com>
2015-10-13 16:21:47 +0200

    Removed obsolete ajax tests

--- test/ajax.js
+++ test/ajax.js
@@ -2,46 +2,6 @@
 describe('h54s', function() {
   describe('Ajax test:', function() {
 
-    it('Test post method', function(done) {
-      this.timeout(10000);
-      var sasAdapter = new h54s();
-      assert.isDefined(sasAdapter._utils.ajax, 'Ajax is not defined');
-      assert.isFunction(sasAdapter._utils.ajax.post, 'Ajax post method is not defined');
-      sasAdapter._utils.ajax.post(serverData.url + 'SASLogon/Logon.do', {
-        _debug: "0",
-        _sasapp: "Stored Process Web App 9.3",
-        _service: "default",
-        ux: serverData.user,
-        px: serverData.pass,
-      }).success(function(res) {
-        assert.isDefined(res, 'Ajax response is undefined');
-        done();
-      }).error(function(res) {
-        assert.notOk(res, 'Ajax got error');
-        done();
-      });
-    });
-
-    it('Test get method', function(done) {
-      this.timeout(10000);
-      var sasAdapter = new h54s();
-      assert.isDefined(sasAdapter._utils.ajax, 'Ajax is not defined');
-      assert.isFunction(sasAdapter._utils.ajax.get, 'Ajax get method is not defined');
-      sasAdapter._utils.ajax.get(serverData.url + 'SASLogon/Logon.do', {
-        _debug: "0",
-        _sasapp: "Stored Process Web App 9.3",
-        _service: "default",
-        ux: serverData.user,
-        px: serverData.pass,
-      }).success(function(res) {
-        assert.isDefined(res, 'Ajax response is undefined');
-        done();
-      }).error(function(res) {
-        assert.notOk(res, 'Ajax got error');
-        done();
-      });
-    });
-
     it('Test timeout', function(done) {
       this.timeout(10000);
       var sasAdapter = new h54s({
