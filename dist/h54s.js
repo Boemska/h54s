52d2fb9d466283f96a2dc8b113f22900670a64d2
Bojan Djurdjevic <bojan88@gmail.com>
2015-10-23 17:09:01 +0200

    Release 0.6.4

--- dist/h54s.js
+++ dist/h54s.js
@@ -1,5 +1,5 @@
-/*! h54s v0.6.3 - 2015-10-09 
- *  License: GPL 
+/*! h54s v0.6.4 - 2015-10-23 
+ *  License: GPL V3 
  *  Author: Boemska 
 */
 if (!Object.create) {
@@ -124,6 +124,38 @@ var h54s = function(config) {
   } else {
     _setConfig.call(this, config);
   }
+
+  // private function to set h54s instance properties
+  function _setConfig(config) {
+    if(!config) {
+      return;
+    } else if(typeof config !== 'object') {
+      throw new h54s.Error('argumentError', 'First parameter should be config object');
+    }
+
+    //merge config object from parameter with this
+    for(var key in config) {
+      if(config.hasOwnProperty(key)) {
+        if((key === 'url' || key === 'loginUrl') && config[key].charAt(0) !== '/') {
+          config[key] = '/' + config[key];
+        }
+        this[key] = config[key];
+      }
+    }
+
+    //if server is remote use the full server url
+    //NOTE: this is not permited by the same-origin policy
+    if(config.hostUrl) {
+      if(config.hostUrl.charAt(config.hostUrl.length - 1) === '/') {
+        config.hostUrl = config.hostUrl.slice(0, -1);
+      }
+      this.hostUrl  = config.hostUrl;
+      this.url      = config.hostUrl + this.url;
+      this.loginUrl = config.hostUrl + this.loginUrl;
+    }
+
+    this._utils.ajax.setTimeout(this.ajaxTimeout);
+  }
 };
 
 /*
@@ -167,43 +199,6 @@ h54s.Tables = function(table, macroName) {
 
   this.add(table, macroName);
 };
-
-/*
-* private function to set h54s instance properties
-*
-*@param {object} config - adapter config object, with keys like url, debug, etc.
-*
-*/
-function _setConfig(config) {
-  if(!config) {
-    return;
-  } else if(typeof config !== 'object') {
-    throw new h54s.Error('argumentError', 'First parameter should be config object');
-  }
-
-  //merge config object from parameter with this
-  for(var key in config) {
-    if(config.hasOwnProperty(key)) {
-      if((key === 'url' || key === 'loginUrl') && config[key].charAt(0) !== '/') {
-        config[key] = '/' + config[key];
-      }
-      this[key] = config[key];
-    }
-  }
-
-  //if server is remote use the full server url
-  //NOTE: this is not permited by the same-origin policy
-  if(config.hostUrl) {
-    if(config.hostUrl.charAt(config.hostUrl.length - 1) === '/') {
-      config.hostUrl = config.hostUrl.slice(0, -1);
-    }
-    this.hostUrl  = config.hostUrl;
-    this.url      = config.hostUrl + this.url;
-    this.loginUrl = config.hostUrl + this.loginUrl;
-  }
-
-  this._utils.ajax.setTimeout(this.ajaxTimeout);
-}
 
 /* global h54s */
 
@@ -316,7 +311,7 @@ h54s.prototype.call = function(sasProgram, tablesObj, callback, params) {
           unescapedResObj = self._utils.unescapeValues(resObj);
         } catch(e) {
           self._utils.parseErrorResponse(res.responseText, sasProgram);
-          callback(new h54s.Error('parseError', 'Unable to parse response json'));
+          callback(new h54s.Error('parseError', e.message));
         } finally {
           if(unescapedResObj) {
             self._utils.addApplicationLogs(resObj.logmessage);
