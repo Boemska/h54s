1b1513f70466e8cfafa5042f3cdeb11b3a17a32d
Bojan Djurdjevic <bojan88@gmail.com>
2015-10-23 16:53:42 +0200

    Moved function because it was polluting global scope

--- src/h54s.js
+++ src/h54s.js
@@ -66,6 +66,38 @@ var h54s = function(config) {
   } else {
     _setConfig.call(this, config);
   }
+
+  // private function to set h54s instance properties
+  function _setConfig(config) {
+    if(!config) {
+      return;
+    } else if(typeof config !== 'object') {
+      throw new h54s.Error('argumentError', 'First parameter should be config object');
+    }
+
+    //merge config object from parameter with this
+    for(var key in config) {
+      if(config.hasOwnProperty(key)) {
+        if((key === 'url' || key === 'loginUrl') && config[key].charAt(0) !== '/') {
+          config[key] = '/' + config[key];
+        }
+        this[key] = config[key];
+      }
+    }
+
+    //if server is remote use the full server url
+    //NOTE: this is not permited by the same-origin policy
+    if(config.hostUrl) {
+      if(config.hostUrl.charAt(config.hostUrl.length - 1) === '/') {
+        config.hostUrl = config.hostUrl.slice(0, -1);
+      }
+      this.hostUrl  = config.hostUrl;
+      this.url      = config.hostUrl + this.url;
+      this.loginUrl = config.hostUrl + this.loginUrl;
+    }
+
+    this._utils.ajax.setTimeout(this.ajaxTimeout);
+  }
 };
 
 /*
@@ -109,40 +141,3 @@ h54s.Tables = function(table, macroName) {
 
   this.add(table, macroName);
 };
-
-/*
-* private function to set h54s instance properties
-*
-*@param {object} config - adapter config object, with keys like url, debug, etc.
-*
-*/
-function _setConfig(config) {
-  if(!config) {
-    return;
-  } else if(typeof config !== 'object') {
-    throw new h54s.Error('argumentError', 'First parameter should be config object');
-  }
-
-  //merge config object from parameter with this
-  for(var key in config) {
-    if(config.hasOwnProperty(key)) {
-      if((key === 'url' || key === 'loginUrl') && config[key].charAt(0) !== '/') {
-        config[key] = '/' + config[key];
-      }
-      this[key] = config[key];
-    }
-  }
-
-  //if server is remote use the full server url
-  //NOTE: this is not permited by the same-origin policy
-  if(config.hostUrl) {
-    if(config.hostUrl.charAt(config.hostUrl.length - 1) === '/') {
-      config.hostUrl = config.hostUrl.slice(0, -1);
-    }
-    this.hostUrl  = config.hostUrl;
-    this.url      = config.hostUrl + this.url;
-    this.loginUrl = config.hostUrl + this.loginUrl;
-  }
-
-  this._utils.ajax.setTimeout(this.ajaxTimeout);
-}
