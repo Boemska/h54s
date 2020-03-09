696629a9bbbf4749b6d795f61c8586528fc2d3f1
bzecevic <bozazec@gmail.com>
2020-03-09 16:39:24 +0100

    fix methods index and ajax

--- src/methods/ajax.js
+++ src/methods/ajax.js
@@ -1,21 +1,23 @@
-module.exports = function() {
+module.exports = function () {
   var timeout = 30000;
   var timeoutHandle;
 
-  var xhr = function(type, url, data, headers = {}, multipartFormData) {
+  var xhr = function (type, url, data, multipartFormData, headers = {}) {
     var methods = {
-      success: function() {},
-      error:   function() {}
+      success: function () {
+      },
+      error: function () {
+      }
     };
 
-    var XHR     = XMLHttpRequest;
+    var XHR = XMLHttpRequest;
     var request = new XHR('MSXML2.XMLHTTP.3.0');
 
     request.open(type, url, true);
 
     //multipart/form-data is set automatically so no need for else block
     // Content-Type header has to be explicitly setted up
-    if(!multipartFormData) {
+    if (!multipartFormData) {
       if (headers['Content-Type']) {
         request.setRequestHeader('Content-Type', headers['Content-Type'])
       } else {
@@ -38,8 +40,8 @@ module.exports = function() {
       }
     };
 
-    if(timeout > 0) {
-      timeoutHandle = setTimeout(function() {
+    if (timeout > 0) {
+      timeoutHandle = setTimeout(function () {
         request.abort();
       }, timeout);
     }
@@ -58,12 +60,12 @@ module.exports = function() {
     };
   };
 
-  const serialize = function(obj) {
+  const serialize = function (obj) {
     var str = [];
-    for(var p in obj) {
+    for (var p in obj) {
       if (obj.hasOwnProperty(p)) {
-        if(obj[p] instanceof Array) {
-          for(var i = 0, n = obj[p].length; i < n; i++) {
+        if (obj[p] instanceof Array) {
+          for (var i = 0, n = obj[p].length; i < n; i++) {
             str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p][i]));
           }
         } else {
@@ -74,16 +76,16 @@ module.exports = function() {
     return str.join("&");
   };
 
-  var createMultipartFormDataPayload = function(obj) {
+  var createMultipartFormDataPayload = function (obj) {
     var data = new FormData();
-    for(var p in obj) {
-      if(obj.hasOwnProperty(p)) {
-        if(obj[p] instanceof Array && p !== 'file') {
-          for(var i = 0, n = obj[p].length; i < n; i++) {
+    for (var p in obj) {
+      if (obj.hasOwnProperty(p)) {
+        if (obj[p] instanceof Array && p !== 'file') {
+          for (var i = 0, n = obj[p].length; i < n; i++) {
             data.append(p, obj[p][i]);
           }
         } else if (p === 'file') {
-					data.append(p, obj[p][0], obj[p][1]);
+          data.append(p, obj[p][0], obj[p][1]);
         } else {
           data.append(p, obj[p]);
         }
@@ -92,16 +94,16 @@ module.exports = function() {
     return data;
   };
 
-	return {
-		get: function (url, data, headers) {
-			var dataStr;
-			if (typeof data === 'object') {
-				dataStr = serialize(data);
-			}
-			var urlWithParams = dataStr ? (url + '?' + dataStr) : url;
-			return xhr('GET', urlWithParams, null, headers, null);
-		},
-		post: function(url, data, headers, multipartFormData) {
+  return {
+    get: function (url, data, multipartFormData, headers) {
+      var dataStr;
+      if (typeof data === 'object') {
+        dataStr = serialize(data);
+      }
+      var urlWithParams = dataStr ? (url + '?' + dataStr) : url;
+      return xhr('GET', urlWithParams, null, multipartFormData, headers);
+    },
+		post: function(url, data, multipartFormData, headers) {
       let payload = data;
       if(typeof data === 'object') {
         if(multipartFormData) {
@@ -110,7 +112,7 @@ module.exports = function() {
           payload = serialize(data);
         }
       }
-      return xhr('POST', url, payload, headers, multipartFormData);
+      return xhr('POST', url, payload, multipartFormData, headers);
     },
     put: function(url, data, multipartFormData, headers) {
       let payload = data;
@@ -121,8 +123,9 @@ module.exports = function() {
       }
       return xhr('PUT', url, payload, multipartFormData, headers);
     },
-		setTimeout: function (t) {
-			timeout = t;
-		}
-	};
+    setTimeout: function (t) {
+      timeout = t;
+    },
+    serialize
+  };
 };
